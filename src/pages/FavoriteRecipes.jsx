5a5ceef7f197c5aa21ed0a8fb68f8f30047931f3
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2022-10-26 20:37:08 -0300

    requisito 50

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getItem } from '../services/LocalStorageFuncs';
 import Header from '../components/Header';
 
+// const copy = require('clipboard-copy');
+
 function FavoriteRecipes() {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    setRecipes(getItem('favoriteRecipes'));
+  }, []);
+
+  console.log(recipes);
+
   return (
-    <Header />
+    <>
+      <Header />
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => setType() }
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-meal-btn"
+        onClick={ () => setType('meal') }
+      >
+        Meals
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => setType('drink') }
+      >
+        Drinks
+      </button>
+      {
+        (recipes !== undefined && recipes.length > 0) && (
+          <ul>
+            {
+              recipes?.map((dr, index) => (
+                <li key={ dr.id }>
+                  <img
+                    src={ dr.image }
+                    alt={ dr.name }
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                  <p data-testid={ `${index}-horizontal-top-text` }>{ dr.category }</p>
+                  <p data-testid={ `${index}-horizontal-name` }>{ dr.name }</p>
+                  <p data-testid={ `${index}-horizontal-done-date` }>{ dr.doneDate }</p>
+                  <button
+                    type="button"
+                    data-testid={ `${index}-horizontal-share-btn` }
+                  >
+                    Compartilhar
+                  </button>
+                  <button
+                    type="button"
+                    data-testid={ `${index}-horizontal-favorite-btn` }
+                  >
+                    favoritar
+                  </button>
+                </li>
+              ))
+            }
+          </ul>
+        )
+      }
+    </>
   );
 }
 
